61aacc22c2238d49158fbbf5dc86a8585c0a9abd
Matt Melito <matt_melito@outlook.com>
2023-04-22 20:33:21 -0700

    corrected indentation and spacing for final submission.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -3,32 +3,32 @@ using Bakery.Models;
 
 namespace Bakery
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Welcome message and prices
-            Console.WriteLine("Welcome to Pierre's Bakery!");
-            Console.WriteLine("Bread: $5 each, or buy 2 get 1 free.");
-            Console.WriteLine("Pastries: $2 each, or buy 3 get 1 free.");
+      // Welcome message and prices
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread: $5 each, or buy 2 get 1 free.");
+      Console.WriteLine("Pastries: $2 each, or buy 3 get 1 free.");
 
-            // // Get user input for number of loaves and pastries
-            Console.Write("How many loaves of bread would you like? ");
-            int breadCount = int.Parse(Console.ReadLine());
+      // // Get user input for number of loaves and pastries
+      Console.Write("How many loaves of bread would you like? ");
+      int breadCount = int.Parse(Console.ReadLine());
 
-            Console.Write("How many pastries would you like? ");
-            int pastryCount = int.Parse(Console.ReadLine());
+      Console.Write("How many pastries would you like? ");
+      int pastryCount = int.Parse(Console.ReadLine());
 
-            // // Create instances of the Bread and Pastry classes
-            Bread bread = new Bread(breadCount);
-            Pastry pastry = new Pastry(pastryCount);
+      // // Create instances of the Bread and Pastry classes
+      Bread bread = new Bread(breadCount);
+      Pastry pastry = new Pastry(pastryCount);
 
-            // // Calculate the total cost of the order
-            decimal totalCost = bread.GetCost() + pastry.GetCost();
+      // // Calculate the total cost of the order
+      decimal totalCost = bread.GetCost() + pastry.GetCost();
 
-            // // Display the total cost to the user
-            Console.WriteLine($"Your total cost is: ${totalCost}");
-            Console.ReadLine();
-        }
+      // // Display the total cost to the user
+      Console.WriteLine($"Your total cost is: ${totalCost}");
+      Console.ReadLine();
     }
+  }
 }
