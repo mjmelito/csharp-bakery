728ef0a6b21e798308a9327a3fd9be21adac40fa
Matt Melito <matt_melito@outlook.com>
2023-04-22 18:16:36 -0700

    Add good fail (red phase) for GetPastryCount test method.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -6,6 +6,20 @@ namespace Bakery.Tests
   [TestClass]
   public class PastryTests
   {
-    // Tests will go here!
+    [TestMethod]
+        public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+        {
+            Pastry newPastry = new Pastry(1);
+            Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+        }
+
+    [TestMethod]
+        public void GetBreadCount_ReturnsBreadCount_Int()
+        {
+            int pastryCount = 3;
+            Pastry newPastry = new Pastry(pastryCount);
+            int result = newPastry.PastryCount;
+            Assert.AreEqual(pastryCount, result);
+        }
   }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,15 @@ namespace Bakery.Models
 {
     public class Pastry
     {
-
+        private int _pastryCount;
+        public int PastryCount
+        {
+            get {return _pastryCount; }
+            set {_pastryCount = value; }
+        }
+        public Pastry(int count)
+        {
+            // _breadCount = count;
+        }
     }
 }
